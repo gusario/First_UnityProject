6b07d203e3d7f9cf91310f1193f614356ae1304e
David <david.nurdinov@icloud.com>
2018-10-22 01:41:59 +0300

    innnitial commit

---
+++ Assets/scripts/FPSInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour {
+
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    private CharacterController _charController;
+	// Use this for initialization
+	void Start () {
+      _charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ); //создание вектора движения, который будет изменяться по оси X и Z
+        movement = Vector3.ClampMagnitude(movement, speed); //ограничение скорости, если идет движение по диагонали
+        movement.y = gravity; //добавление гравитации
+        movement *= Time.deltaTime; //отвязка от FPS
+        movement = transform.TransformDirection(movement); // перевод координат, чтобы нормально двигаться
+        _charController.Move(movement); // само движение 
+	}
+}
---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public enum RotationAxis
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxis axes = RotationAxis.MouseXAndY;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float _rotationX = 0;
+	// Use this for initialization
+	void Start () {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+            body.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (axes == RotationAxis.MouseX) {
+            //поворот по горизонтали
+            transform.Rotate(0,Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxis.MouseY) {
+            //Поворот по вертикали
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else {
+            //Комбинированный поворот
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+	}
+}
